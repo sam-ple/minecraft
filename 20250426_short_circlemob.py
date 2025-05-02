4ac05d7bbde999a214b2fd2cc3f1f4a07c2af9d6
くろ <451023+sam-ple@users.noreply.github.com>
2025-05-02 17:29:44 +0900

    Update 20250426_short_circlemob.py

--- 20250426_short_circlemob.py
+++ 20250426_short_circlemob.py
@@ -1,33 +1,20 @@
 # -------------------------
 # 標準ライブラリ
 # -------------------------
-from sys import argv, exit
-from time import sleep
-import random
+import time
 import math
-from datetime import datetime
 
 # -------------------------
 # 外部ライブラリ
 # -------------------------
 import minescript
-from minescript import execute, echo
-
-# -------------------------
-# 引数チェック
-# -------------------------
-if len(argv) <= 1:
-    echo("コマンドを指定してください。")
-    exit(1)
-
-arg1 = argv[1]
-arg2 = argv[2] if len(argv) > 2 else None
+from minescript import execute, echo, player_position
 
 # -------------------------
 # モブ召喚関数
 # -------------------------
 def summon_circle(entity, sound=None, count=12, radius=1):
-    x, y, z = minescript.player_position()
+    x, y, z = map(int, player_position())
 
     for i in range(count):
         angle = 2 * math.pi * i / count
@@ -42,18 +29,11 @@ def summon_circle(entity, sound=None, count=12, radius=1):
     if sound:
         execute(f"playsound {sound} master @a ~ ~ ~ 1 1 1")
 
-    sleep(5)
+    time.sleep(5)
 
-# ------------------------------------------------------------
-# コマンド分岐
-# ------------------------------------------------------------
-if arg1 == "return":
-    execute(f"/tp @p -12 64 -136")
-
-elif arg1 == "mobs":
-    summon_circle("minecraft:allay", "minecraft:entity.allay.ambient_with_item")
-    summon_circle("minecraft:bee", "minecraft:entity.bee.ambient")
-    summon_circle("minecraft:parrot", "minecraft:entity.parrot.ambient")
-
-else:
-    echo(f"未対応のコマンドです: {arg1}")
+# -------------------------
+# 呼び出し
+# -------------------------
+summon_circle("minecraft:allay", "minecraft:entity.allay.ambient_with_item")
+summon_circle("minecraft:bee", "minecraft:entity.bee.ambient")
+summon_circle("minecraft:parrot", "minecraft:entity.parrot.ambient")
