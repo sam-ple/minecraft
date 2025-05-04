2e57102eaa3b4248b8b927b2b6410d69369a3450
くろ <451023+sam-ple@users.noreply.github.com>
2025-05-04 09:48:50 +0900

    Update 20250425_short_stroll.py

--- 20250425_short_stroll.py
+++ 20250425_short_stroll.py
@@ -15,22 +15,28 @@ from minescript import execute, echo, player_position
 x, y, z = map(int, player_position())
 
 # -------------------------
-# アレイに乗って空中散歩
-# -------------------------
-execute(f'/summon minecraft:allay {x} {y+1} {z} {{Invulnerable:1b,Glowing:1b}}')
-execute('/attribute @p minecraft:generic.scale base set 0.5')
-execute('/ride @p mount @e[type=allay,limit=1,sort=nearest,distance=..3]')
-echo("ちっちゃくなってアレイに乗って空中散歩へ…！")
-
-time.sleep(10)
-
-# -------------------------
 # 巨大エンダーマンに乗る
 # -------------------------
-execute(f'/summon minecraft:enderman {x} {y+1} {z} {{Invulnerable:1b,Glowing:1b}}')
-execute('/execute as @e[type=enderman,limit=1,sort=nearest,distance=..5] run attribute @s minecraft:generic.scale base set 10')
-execute('/attribute @p minecraft:generic.scale base set 1')
+execute('/attribute @p minecraft:scale base set 1')
+execute(f'/summon minecraft:enderman {x} {y+1} {z} {{Invulnerable:1b}}')
+execute('/execute as @e[type=enderman,limit=1,sort=nearest,distance=..5] run attribute @s minecraft:scale base set 3')
+time.sleep(1)
 execute('/ride @p mount @e[type=enderman,limit=1,sort=nearest,distance=..5]')
 echo("巨大なエンダーマンに乗って異世界の旅へ…！")
 
 time.sleep(10)
+execute('/ride @p dismount')
+time.sleep(1)
+
+# -------------------------
+# アレイに乗って空中散歩
+# -------------------------
+execute('/attribute @p minecraft:scale base set 0.5')
+execute(f'/summon minecraft:allay {x} {y+1} {z} {{Invulnerable:1b}}')
+execute('/ride @p mount @e[type=allay,limit=1,sort=nearest,distance=..3]')
+time.sleep(1)
+echo("ちっちゃくなってアレイに乗って空中散歩へ…！")
+
+time.sleep(10)
+execute('/ride @p dismount')
+time.sleep(1)
